7212e00db96247ce3e58e079004133e94fdb1590
miao <chuoli223@yahoo.com>
2023-11-11 18:00:10 +0100

    update exp1 plot, correct groupby data, averaging errors.

--- src/plot/rm_bar_exp1_plot.R
+++ src/plot/rm_bar_exp1_plot.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(readxl)
+install.packages('rstatix')
+library(rstatix)
 
 # ---------------read data------------------------
 
@@ -17,23 +19,24 @@ my_data <- readxl::read_excel(file.choose())
 
 # -------------Deivation - set size-------------------
 
-data_across_pp <- my_data %>%
-  group_by(correct_num, trial_type) %>%
-  summarise(
-    num_dv_mean = mean(number_deviation),
-    num_dv_sd = sd(number_deviation),
-    n = n()
-  ) %>%
-  mutate(
-    num_dv_SEM = num_dv_sd / sqrt(n),
-    num_dv_CI = num_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
-  )
-
 data_by_pp <- my_data %>%
-  group_by(correct_num, trial_type, subID) %>%
+  group_by(correct_num, correct_width, subID) %>%
   summarise(
-    num_dv_mean = mean(number_deviation),
-    num_dv_sd = sd(number_deviation),
+    number_deviation_mean = mean(number_deviation),
+    number_deviation_sd = sd(number_deviation),
+    n = n()
+  ) %>%
+  mutate(
+    num_deviation_SEM = number_deviation_sd / sqrt(n),
+    num_deviation_CI = num_deviation_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
+  )
+
+
+data_across_pp <- data_by_pp %>%
+  group_by(correct_num, correct_width) %>%
+  summarise(
+    num_dv_mean = mean(number_deviation_mean),
+    num_dv_sd = sd(number_deviation_mean),
     n = n()
   ) %>%
   mutate(
@@ -41,6 +44,8 @@ data_by_pp <- my_data %>%
     num_dv_CI = num_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
   )
 
+data_by_pp$correct_width <- as.factor(data_by_pp$correct_width)
+data_across_pp$correct_width <- as.factor(data_across_pp$correct_width)
 
 my_plot <-  ggplot() +
   
@@ -49,8 +54,8 @@ my_plot <-  ggplot() +
     aes(
       x = correct_num,
       y = num_dv_mean,
-      group = trial_type,
-      color = trial_type
+      group = correct_width,
+      color = correct_width
     ),
     position = position_dodge(0.2),
     stat = "identity",
@@ -59,12 +64,12 @@ my_plot <-  ggplot() +
   
   
   # each data point represents the average deviation of 1 participant
-  # geom_point(data = data_by_pp, aes(x = correct_num,
-  #                                        y = num_dv_mean,
-  #                                        group = correct_width,
-  #                                        color = correct_space),
-  #            alpha = 0.2,
-  #            position = position_dodge(0.2))+
+  geom_point(data = data_by_pp, aes(x = correct_num,
+                                         y = number_deviation_mean,
+                                         group = correct_width,
+                                         color = correct_width),
+             alpha = 0.2,
+             position = position_dodge(0.2))+
   
   geom_errorbar(
     data = data_across_pp,
@@ -73,8 +78,8 @@ my_plot <-  ggplot() +
       y = num_dv_mean,
       ymin = num_dv_mean - num_dv_SEM,
       ymax = num_dv_mean + num_dv_SEM,
-      group = trial_type,
-      color = trial_type
+      group = correct_width,
+      color = correct_width
     ),
     
     size  = 1.2,
@@ -122,16 +127,31 @@ my_plot <-  ggplot() +
 
 my_plot
 
-
 # -------------Width dv - set size-------------------
-
-data_across_pp <- my_data %>%
-  group_by(correct_num, RM, correct_width) %>%
+data_by_pp2 <- my_data %>%
+  group_by(correct_num, correct_width, RM, subID) %>%
   summarise(
-    width_dv_mean = mean(width_deviation),
-    width_dv_sd = sd(width_deviation),
-    spacing_dv_mean = mean(space_deviation),
-    spacing_dv_sd = sd(space_deviation),
+    width_deviation_mean = mean(width_deviation),
+    width_deviation_sd = sd(width_deviation),
+    spacing_deviation_mean = mean(space_deviation),
+    spacing_deviation_sd = sd(space_deviation),
+    n = n()
+  ) %>%
+  mutate(
+    width_deviation_SEM = width_deviation_sd / sqrt(n),
+    width_deviation_CI = width_deviation_SEM * qt((1 - 0.05) / 2 + .5, n - 1),
+    spacing_deviation_SEM = spacing_deviation_sd / sqrt(n),
+    spacing_deviation_CI = spacing_deviation_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
+  )
+
+
+data_across_pp2 <- data_by_pp2 %>%
+  group_by(correct_num, correct_width, RM) %>%
+  summarise(
+    width_dv_mean = mean(width_deviation_mean),
+    width_dv_sd = sd(width_deviation_mean),
+    spacing_dv_mean = mean(spacing_deviation_mean),
+    spacing_dv_sd = sd(spacing_deviation_mean),
     n = n()
   ) %>%
   mutate(
@@ -142,10 +162,13 @@ data_across_pp <- my_data %>%
   )
 
 
-my_plot <-  ggplot() +
+data_by_pp2$correct_width <- as.factor(data_by_pp2$correct_width)
+data_across_pp2$correct_width <- as.factor(data_across_pp2$correct_width)
+
+my_plot2 <-  ggplot() +
   
   geom_point(
-    data = data_across_pp,
+    data = data_across_pp2,
     aes(
       x = correct_num,
       y = width_dv_mean,
@@ -153,36 +176,38 @@ my_plot <-  ggplot() +
       color = RM,
       size = n
     ),
-    position = position_dodge(0.2),
+    position = position_dodge(0.5),
     stat = "identity",
-    alpha = 0.5
+    alpha = 0.8,
   ) +
   
   
   # each data point represents the average deviation of 1 participant
-  # geom_point(data = data_by_pp, aes(x = correct_num,
-  #                                        y = num_dv_mean,
-  #                                        group = correct_width,
-  #                                        color = correct_space),
-  #            alpha = 0.2,
-  #            position = position_dodge(0.2))+
+  geom_point(data = data_by_pp2, aes(x = correct_num,
+                                         y = width_deviation_mean,
+                                         group = RM,
+                                         color = RM,
+                                     size = n),
+             alpha = 0.1,
+             position = position_dodge(0.5))+
+
   
   geom_errorbar(
-    data = data_across_pp,
+    data = data_across_pp2,
     aes(
       x = correct_num,
       y = width_dv_mean,
       group = RM,
-      color = RM,
       ymin = width_dv_mean - width_dv_SEM,
       ymax = width_dv_mean + width_dv_SEM,
 
     ),
     
-    size  = 1.2,
+    size  = 1.0,
     width = .00,
     alpha = 0.5,
-    position = position_dodge(0.2)
+    color = "black",
+    position = position_dodge(0.5)
   ) +
   
   geom_hline(yintercept = 0, linetype = "dashed") +
@@ -222,38 +247,24 @@ my_plot <-  ggplot() +
     strip.text.x = element_text(size = 12, face = "bold")
   ) +
   
-  facet_wrap( ~ correct_width, labeller = labeller(correct_width =
-                                                     c(
-                                                       "0.1" = "actual width 0.1",
-                                                       "0.25" = "actual width 0.25",
-                                                       "0.4" = "actual width 0.4"
-                                                     ))
-              )
+  facet_wrap(~ correct_width, labeller = labeller(
+    correct_width =
+      c(
+        "0.1" = "actual width 0.1",
+        "0.25" = "actual width 0.25",
+        "0.4" = "actual width 0.4"
+      )
+  )
+  )
 
-my_plot
+my_plot2
 
 
 # -------------spacing dv - set size for each correct spacing and width-----
-data_across_pp <- my_data %>%
-  group_by(correct_num, RM, correct_width, correct_space) %>%
-  summarise(
-    width_dv_mean = mean(width_deviation),
-    width_dv_sd = sd(width_deviation),
-    spacing_dv_mean = mean(space_deviation),
-    spacing_dv_sd = sd(space_deviation),
-    n = n()
-  ) %>%
-  mutate(
-    width_dv_SEM = width_dv_sd / sqrt(n),
-    width_dv_CI = width_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1),
-    spacing_dv_SEM = spacing_dv_sd / sqrt(n),
-    spacing_dv_CI = spacing_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
-  )
-
-my_plot <-  ggplot() +
+my_plot2.1 <-  ggplot() +
   
   geom_point(
-    data = data_across_pp,
+    data = data_across_pp2,
     aes(
       x = correct_num,
       y = spacing_dv_mean,
@@ -261,27 +272,32 @@ my_plot <-  ggplot() +
       color = RM,
       size = n
     ),
-    position = position_dodge(0.2),
+    position = position_dodge(0.5),
     stat = "identity",
-    alpha = 0.5
+    alpha = 0.8
   ) +
   
   
   # each data point represents the average deviation of 1 participant
-  # geom_point(data = data_by_pp, aes(x = correct_num,
-  #                                        y = num_dv_mean,
-  #                                        group = correct_width,
-  #                                        color = correct_space),
-  #            alpha = 0.2,
-  #            position = position_dodge(0.2))+
-  
+  geom_point(
+    data = data_by_pp2,
+    aes(
+      x = correct_num,
+      y = spacing_deviation_mean,
+      group = RM,
+      color = RM,
+      size = n
+    ),
+    alpha = 0.1,
+    position = position_dodge(0.5)
+  )+
+
   geom_errorbar(
-    data = data_across_pp,
+    data = data_across_pp2,
     aes(
       x = correct_num,
       y = spacing_dv_mean,
       group = RM,
-      color = RM,
       ymin = spacing_dv_mean - spacing_dv_SEM,
       ymax = spacing_dv_mean + spacing_dv_SEM,
       
@@ -290,116 +306,8 @@ my_plot <-  ggplot() +
     size  = 1.2,
     width = .00,
     alpha = 0.5,
-    position = position_dodge(0.2)
-  ) +
-  
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  
-  labs(y = "Spacing deviation (°)", x = "Set size") +
-  
-  scale_x_continuous(breaks = c(3, 4, 5), 
-                     labels = c("3", "4", "5"), limits = c(2.5, 5.5))+
-  
-  
-  theme(
-    axis.title.x = element_text(
-      color = "black",
-      size = 14,
-      face = "bold"
-    ),
-    axis.title.y = element_text(
-      color = "black",
-      size = 14,
-      face = "bold"
-    ),
-    panel.border = element_blank(),
-    # remove panel grid lines
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    # remove panel background
-    panel.background = element_blank(),
-    # add axis line
-    axis.line = element_line(colour = "grey"),
-    # x,y axis tick labels
-    axis.text.x = element_text(size = 12, face = "bold"),
-    axis.text.y = element_text(size = 12, face = "bold"),
-    # legend size
-    legend.title = element_text(size = 12, face = "bold"),
-    legend.text = element_text(size = 10),
-    # facet wrap title
-    strip.text.x = element_text(size = 12, face = "bold")
-  ) +
-  
-  facet_wrap( ~ correct_width + correct_space, labeller = labeller(correct_width =
-                                                     c(
-                                                       "0.1" = "actual width 0.1",
-                                                       "0.25" = "actual width 0.25",
-                                                       "0.4" = "actual width 0.4"
-                                                     ))
-  )
-
-my_plot
-
-
-
-# -------------spacing dv - set size for each correct width-----
-data_across_pp <- my_data %>%
-  group_by(correct_num, RM, correct_width) %>%
-  summarise(
-    width_dv_mean = mean(width_deviation),
-    width_dv_sd = sd(width_deviation),
-    spacing_dv_mean = mean(space_deviation),
-    spacing_dv_sd = sd(space_deviation),
-    n = n()
-  ) %>%
-  mutate(
-    width_dv_SEM = width_dv_sd / sqrt(n),
-    width_dv_CI = width_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1),
-    spacing_dv_SEM = spacing_dv_sd / sqrt(n),
-    spacing_dv_CI = spacing_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
-  )
-
-my_plot <-  ggplot() +
-  
-  geom_point(
-    data = data_across_pp,
-    aes(
-      x = correct_num,
-      y = spacing_dv_mean,
-      group = RM,
-      color = RM,
-      size = n
-    ),
-    position = position_dodge(0.2),
-    stat = "identity",
-    alpha = 0.5
-  ) +
-  
-  
-  # each data point represents the average deviation of 1 participant
-  # geom_point(data = data_by_pp, aes(x = correct_num,
-  #                                        y = num_dv_mean,
-  #                                        group = correct_width,
-  #                                        color = correct_space),
-  #            alpha = 0.2,
-  #            position = position_dodge(0.2))+
-  
-  geom_errorbar(
-    data = data_across_pp,
-    aes(
-      x = correct_num,
-      y = spacing_dv_mean,
-      group = RM,
-      color = RM,
-      ymin = spacing_dv_mean - spacing_dv_SEM,
-      ymax = spacing_dv_mean + spacing_dv_SEM,
-      
-    ),
-    
-    size  = 1.2,
-    width = .00,
-    alpha = 0.5,
-    position = position_dodge(0.2)
+    color = "black",
+    position = position_dodge(0.5)
   ) +
   
   geom_hline(yintercept = 0, linetype = "dashed") +
@@ -448,16 +356,34 @@ my_plot <-  ggplot() +
       )
   ))
 
-my_plot
-# -------------Deviation width - Deviation Num --------
+my_plot2.1
 
-data_across_pp <- my_data %>%
+
+# -------------Deviation width - Deviation Num --------
+data_by_pp3 <- my_data %>%
+  group_by(correct_num, correct_width, number_deviation, subID) %>%
+  summarise(
+    width_deviation_mean = mean(width_deviation),
+    width_deviation_sd = sd(width_deviation),
+    spacing_deviation_mean = mean(space_deviation),
+    spacing_deviation_sd = sd(space_deviation),
+    n = n()
+  ) %>%
+  mutate(
+    width_deviation_SEM = width_deviation_sd / sqrt(n),
+    width_deviation_CI = width_deviation_SEM * qt((1 - 0.05) / 2 + .5, n - 1),
+    spacing_deviation_SEM = spacing_deviation_sd / sqrt(n),
+    spacing_deviation_CI = spacing_deviation_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
+  )
+
+
+data_across_pp3 <- data_by_pp3 %>%
   group_by(correct_num, correct_width, number_deviation) %>%
   summarise(
-    width_dv_mean = mean(width_deviation),
-    width_dv_sd = sd(width_deviation),
-    spacing_dv_mean = mean(space_deviation),
-    spacing_dv_sd = sd(space_deviation),
+    width_dv_mean = mean(width_deviation_mean),
+    width_dv_sd = sd(width_deviation_mean),
+    spacing_dv_mean = mean(spacing_deviation_mean),
+    spacing_dv_sd = sd(spacing_deviation_mean),
     n = n()
   ) %>%
   mutate(
@@ -467,77 +393,59 @@ data_across_pp <- my_data %>%
     spacing_dv_CI = spacing_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
   )
 
-data_by_pp <- my_data %>%
-  group_by(correct_num, correct_width, number_deviation,subID) %>%
-  summarise(
-    width_dv_mean = mean(width_deviation),
-    width_dv_sd = sd(width_deviation),
-    n = n()
-  ) %>%
-  mutate(
-    width_dv_SEM = width_dv_sd / sqrt(n),
-    width_dv_CI = width_dv_SEM * qt((1 - 0.05) / 2 + .5, n - 1)
-    )
+data_by_pp3$correct_width <- as.factor(data_by_pp3$correct_width)
+data_across_pp3$correct_width <- as.factor(data_across_pp3$correct_width)
 
 
-my_plot <-  ggplot() +
+my_plot3 <-  ggplot() +
   
   geom_point(
-    data = data_across_pp,
+    data = data_across_pp3,
     aes(
       x = number_deviation,
-      y = width_dv_mean
+      y = width_dv_mean,
+      color = as.factor(number_deviation),
+      size = n
     ),
     position = position_dodge(0.2),
     stat = "identity",
-    alpha = 0.5
+    alpha = 0.8
   ) +
   
   
-  # each data point represents the average deviation of 1 participant
-  # geom_point(data = data_by_pp, aes(x = correct_num,
-  #                                        y = num_dv_mean,
-  #                                        group = correct_width,
-  #                                        color = correct_space),
-  #            alpha = 0.2,
-  #            position = position_dodge(0.2))+
-  
+  geom_point(
+    data = data_by_pp3,
+    aes(x = number_deviation,
+        y = width_deviation_mean,
+        color = as.factor(number_deviation),
+        size = n),
+    alpha = 0.1,
+    position = position_dodge(0.2)
+  )+
+
   geom_errorbar(
-    data = data_across_pp,
+    data = data_across_pp3,
     aes(
       x = number_deviation,
       y = width_dv_mean,
       ymin = width_dv_mean - width_dv_SEM,
-      ymax = width_dv_mean + width_dv_SEM,
-      group = correct_width
+      ymax = width_dv_mean + width_dv_SEM
     ),
 
-    size  = 1.2,
+    size  = 1.0,
     width = .00,
     alpha = 0.5,
+    color = "black",
     position = position_dodge(0.2)
   ) +
   
-  stat_smooth(
-    data = data_across_pp,
-    aes(
-      x = number_deviation,
-      y = width_dv_mean,
-      group = correct_width
-    ),
-    method = "lm",
-    size = 0.5,
-    se = FALSE,
-    alpha = 0.5,
-    geom = "line"
-  )+
 
   geom_hline(yintercept = 0, linetype = "dashed") +
   
   labs(y = "Width deviation (°)", x = "Deviation (number task)") +
   
-  scale_x_continuous(breaks = c(-4, -3, -2, -1, 0, 1, 2, 3, 4), 
-                     labels = c("-4", "-3", "-2", "-1", "0", "1", "2", "3", "4"), limits = c(-4.5, 4.5))+
+  scale_x_continuous(breaks = c( -3, -2, -1, 0, 1, 2, 3), 
+                     labels = c("-3", "-2", "-1", "0", "1", "2", "3"), limits = c(-4.5, 4.5))+
   
   
   theme(
@@ -584,17 +492,19 @@ my_plot <-  ggplot() +
               ))
     
 
-print(my_plot)
+my_plot3
 
 # -------------Deviation spacing - Deviation Num --------
 
-my_plot <-  ggplot() +
+my_plot3.1 <-  ggplot() +
   
   geom_point(
-    data = data_across_pp,
+    data = data_across_pp3,
     aes(
       x = number_deviation,
-      y = spacing_dv_mean
+      y = spacing_dv_mean,
+      color = as.factor(number_deviation),
+      size = n
     ),
     position = position_dodge(0.2),
     stat = "identity",
@@ -603,15 +513,20 @@ my_plot <-  ggplot() +
   
   
   # each data point represents the average deviation of 1 participant
-  # geom_point(data = data_by_pp, aes(x = correct_num,
-  #                                        y = num_dv_mean,
-  #                                        group = correct_width,
-  #                                        color = correct_space),
-  #            alpha = 0.2,
-  #            position = position_dodge(0.2))+
-  
+  geom_point(
+    data = data_by_pp3,
+    aes(
+      x = number_deviation,
+      y = spacing_deviation_mean,
+      color = as.factor(number_deviation),
+      size = n
+    ),
+    alpha = 0.2,
+    position = position_dodge(0.2)
+  )+
+
   geom_errorbar(
-    data = data_across_pp,
+    data = data_across_pp3,
     aes(
       x = number_deviation,
       y = spacing_dv_mean,
@@ -630,8 +545,8 @@ my_plot <-  ggplot() +
   
   labs(y = "Spacing deviation (°)", x = "Deviation (number task)") +
   
-  scale_x_continuous(breaks = c(-4, -3, -2, -1, 0, 1, 2, 3, 4), 
-                     labels = c("-4", "-3", "-2", "-1", "0", "1", "2", "3", "4"), limits = c(-4.5, 4.5))+
+  scale_x_continuous(breaks = c(-3, -2, -1, 0, 1, 2, 3), 
+                     labels = c("-3", "-2", "-1", "0", "1", "2", "3"), limits = c(-4.5, 4.5))+
   
   
   theme(
@@ -678,7 +593,7 @@ my_plot <-  ggplot() +
               ))
 
 
-print(my_plot)
+my_plot3.1
 
 
 
